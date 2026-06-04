0cebfca2b18aa29912e10d6d16b034546a8f80a1
mehernigar17 <mehernigar1744@gmail.com>
2026-06-04 22:40:23 +0600

    without logout

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,20 +1,66 @@
 import 'package:flutter/material.dart';
+import '../widgets/profile_head.dart';
+import 'package:pcos_app_v2/widgets/profile_stats.dart';
+import 'package:pcos_app_v2/widgets/profile_cycle.dart';
+import 'package:pcos_app_v2/widgets/profile_goal.dart';
+import 'package:pcos_app_v2/widgets/profile_prefenrences.dart';
+import 'package:pcos_app_v2/widgets/profile_achievements.dart';
+import 'package:pcos_app_v2/widgets/profile_logout.dart';
 
-class ProfileScreen extends StatefulWidget {
+class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
-  State<ProfileScreen> createState() => _ProfileScreenState();
-}
-
-class _ProfileScreenState extends State<ProfileScreen> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Profile"),
-        centerTitle: true,
+      backgroundColor: const Color(0xFFF9F4FB),
+
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+
+            child: Column(
+              children: [
+                ProfileHeader(),
+                SizedBox(height: 16),
+                ProfileStats(),
+                SizedBox(height: 16),
+                CycleProfileCard(
+                  avgCycleLength: "30 Days",
+                  lastPeriod: "April 2, 2026",
+                  cycleType: "Irregular",
+                  pcosDiagnosis: "Confirmed",
+                ),
+                SizedBox(height: 16),
+                GoalsCard(
+                  goals: [
+                    " Fertility Support",
+                    " Weight Management",
+                    "Better Mood",
+                  ],
+                ),
+                SizedBox(height: 16),
+                PreferencesCard(
+                  dailyReminders: true,
+                  notificationsEnabled: true,
+                  privacyEnabled: false,
+                ),
+                SizedBox(height: 16),
+                AchievementsCard(),
+                SizedBox(height: 16),
+                SignOutButton(
+                  onTap: () {
+                    debugPrint("Sign out pressed");
+                  },
+                ),
+                SizedBox(height: 16),
+              ],
+            ),
+          ),
+        ),
       ),
     );
+
   }
 }
