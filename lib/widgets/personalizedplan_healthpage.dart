fe44f7331d010b27432be290815eba33296c6464
mehernigar17 <mehernigar1744@gmail.com>
2026-06-14 13:44:47 +0600

    personalized plan done

--- lib/widgets/personalizedplan_healthpage.dart
+++ lib/widgets/personalizedplan_healthpage.dart
@@ -64,6 +64,20 @@ class PersonalizedplanHealthpage extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 16),
+          SizedBox(
+            height: 220,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: plans.length,
+              itemBuilder: (context, index) {
+                return PlanCard(plan: plans[index]);
+              },
+            ),
+          ),
+
+
+
+
         ],
       ),
 
@@ -82,6 +96,67 @@ const PlanCard({required this.plan});
 @override
 Widget build(BuildContext context) {
   return Container(
+    width: 180,
+    padding: EdgeInsets.all(12),
+    margin: EdgeInsets.only(right: 10),
+    decoration: BoxDecoration(
+        color: const Color(0xFF9B7FD4).withOpacity(0.5),
+      borderRadius: BorderRadius.circular(14),
+
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        //icon
+        Icon(
+          plan.icon,
+          color: Colors.white,
+
+        ),
+        //title
+        Text(
+          plan.title,
+          style: GoogleFonts.poppins(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 13,
+          ),
+        ),
+        //listof items
+        const SizedBox(height: 8),
+
+        Expanded(
+          child: ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: plan.item.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 4),
+                child: Row(
+                  children: [
+                    const Text(
+                      "• ",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Expanded(
+                      child: Text(
+                        plan.item[index],
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+
+      ],
+    ),
 
 
 
